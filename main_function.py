c3a3db7a706fc02da8417ea62003bfe6d40f7a40
kathlandgren <ek672@cornell.edu>
2021-06-22 19:25:13 -0600

    specify R in main_script

--- main_function.py
+++ main_function.py
@@ -536,6 +536,8 @@ def main(M,dt,tmax,Phibar, omega, a, test, g=9.8, forcflag=1, taurad=86400, taud
         Cmdata[t,:,:]=rfl.fwd_fft_trunc(C, I, M)
         Dmdata[t,:,:]=rfl.fwd_fft_trunc(D, I, M)
         Emdata[t,:,:]=rfl.fwd_fft_trunc(E, I, M)
+
+        print(dt)
         
         
 
