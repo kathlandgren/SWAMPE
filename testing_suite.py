9b36cd14adadf0b06bc3853b58bca897f972da5f
kathlandgren <ek672@cornell.edu>
2022-03-31 13:01:07 -0400

    Update testing_suite.py

--- testing_suite.py
+++ testing_suite.py
@@ -39,81 +39,95 @@ dt=100 #dt/10
 # Associated Legendre Polynomials and their derivatives
 Pmn, Hmn = rfl.PmnHmn(J, M, N, mus)
 
-SU0, sina, cosa, etaamp=ic.test1_init(a, omega, a1)
-etaic0, etaic1, deltaic0, deltaic1, Phiic0, Phiic1=ic.state_var_init(I,J,mus,lambdas,a,sina,cosa,etaamp,test)
-Uic,Vic=ic.velocity_init(I,J,SU0,cosa,sina,mus,lambdas,test)
-Aic,Bic,Cic,Dic,Eic=ic.ABCDE_init(Uic,Vic,etaic0,Phiic0,mus,I,J)
+#SU0, sina, cosa, etaamp=ic.test1_init(a, omega, a1)
+#etaic0, etaic1, deltaic0, deltaic1, Phiic0, Phiic1=ic.state_var_init(I,J,mus,lambdas,a,sina,cosa,etaamp,test)
+#Uic,Vic=ic.velocity_init(I,J,SU0,cosa,sina,mus,lambdas,test)
+#Aic,Bic,Cic,Dic,Eic=ic.ABCDE_init(Uic,Vic,etaic0,Phiic0,mus,I,J)
 
 
-fmn=np.zeros([M+1,N+1]) #TODO make a function in tstep
-fmn[0,1]=omega/np.sqrt(0.375)
+# fmn=np.zeros([M+1,N+1]) #TODO make a function in tstep
+# fmn[0,1]=omega/np.sqrt(0.375)
 
-tstepcoeffmn=tstep.tstepcoeffmn(M,N,a)
-tstepcoeff=tstep.tstepcoeff(J,M,dt,mus,a)
-tstepcoeff2=tstep.tstepcoeff2(J,M,dt,a)
-mJarray=tstep.mJarray(J,M)
-marray=tstep.marray(M, N)
-narray=tstep.narray(M,N)
+# tstepcoeffmn=tstep.tstepcoeffmn(M,N,a)
+# tstepcoeff=tstep.tstepcoeff(J,M,dt,mus,a)
+# tstepcoeff2=tstep.tstepcoeff2(J,M,dt,a)
+# mJarray=tstep.mJarray(J,M)
+# marray=tstep.marray(M, N)
+# narray=tstep.narray(M,N)
 
 
-def transform_test(U,I,J,M,N,Pmn,w,mus,lambdas):
-    ## This test takes a scalar field, transforms is to spectral space and back
-    Um=rfl.fwd_fft_trunc(U,I,M)
+def init_test():
+    
+    N,I,J,dt,K4,lambdas,mus,w=ic.spectral_params(42)
+    
+    assert N==42, "N should be 42"
+    assert I==128, "I should be 128"
+    assert J==64, "J should be 64"   
+
+
+if __name__=='__main__':
+    init_test()
+    print('All tests passed')
+    
+
+# def transform_test(U,I,J,M,N,Pmn,w,mus,lambdas):
+#     ## This test takes a scalar field, transforms is to spectral space and back
+#     Um=rfl.fwd_fft_trunc(U,I,M)
 
      
-    Umn=rfl.fwd_leg(Um,J,M,N,Pmn,w)
+#     Umn=rfl.fwd_leg(Um,J,M,N,Pmn,w)
         
-    temp, Umnew=rfl.invrs_leg(Umn,I,J,M,N,Pmn)
-    print(np.shape(Umnew))
+#     temp, Umnew=rfl.invrs_leg(Umn,I,J,M,N,Pmn)
+#     print(np.shape(Umnew))
     
-    Unew=rfl.invrs_fft(Umnew,I)
+#     Unew=rfl.invrs_fft(Umnew,I)
     
-    #plotting
-    plt.contourf(lambdas, mus, U-Unew)
-    plt.colorbar()
-    plt.title('U error')
-    plt.show()
+#     #plotting
+#     plt.contourf(lambdas, mus, U-Unew)
+#     plt.colorbar()
+#     plt.title('U error')
+#     plt.show()
     
-    #plotting
-    plt.contourf(lambdas, mus, U)
-    plt.colorbar()
-    plt.title('U IC')
-    plt.show()
+#     #plotting
+#     plt.contourf(lambdas, mus, U)
+#     plt.colorbar()
+#     plt.title('U IC')
+#     plt.show()
     
-    plt.contourf(lambdas, mus, Unew)
-    plt.colorbar()
-    plt.title('Transform')
-    plt.show()
+#     plt.contourf(lambdas, mus, Unew)
+#     plt.colorbar()
+#     plt.title('Transform')
+#     plt.show()
     
-    return Unew
+#     return Unew
 
-def wind_test(U,V,I,J,M,N,Pmn,Hmn,w,tstepcoeff,tstepcoeffmn,mJarray,marray,dt,fmn):
-   ## This test takes a wind field, converts it to vorticity and divergence and recreates the wind field 
-    Um=rfl.fwd_fft_trunc(U,I,M)
-    Vm=rfl.fwd_fft_trunc(V,I, M)
+# def wind_test(U,V,I,J,M,N,Pmn,Hmn,w,tstepcoeff,tstepcoeffmn,mJarray,marray,dt,fmn):
+#    ## This test takes a wind field, converts it to vorticity and divergence and recreates the wind field 
+#     Um=rfl.fwd_fft_trunc(U,I,M)
+#     Vm=rfl.fwd_fft_trunc(V,I, M)
 
-    eta,delta,etamn,deltamn=rfl.diagnostic_eta_delta(Um,Vm, fmn,I,J,M,N,Pmn,Hmn,w,tstepcoeff,mJarray,dt)    #why does it need a dt?
+#     eta,delta,etamn,deltamn=rfl.diagnostic_eta_delta(Um,Vm, fmn,I,J,M,N,Pmn,Hmn,w,tstepcoeff,mJarray,dt)    #why does it need a dt?
         
-    Unew,Vnew=rfl.invrsUV(deltamn,etamn,fmn,I,J,M,N,Pmn,Hmn,tstepcoeffmn,marray)
+#     Unew,Vnew=rfl.invrsUV(deltamn,etamn,fmn,I,J,M,N,Pmn,Hmn,tstepcoeffmn,marray)
     
-        #plotting
-    plt.contourf(lambdas, mus, U-Unew)
-    plt.colorbar()
-    plt.title('U error')
-    plt.show()
+#         #plotting
+#     plt.contourf(lambdas, mus, U-Unew)
+#     plt.colorbar()
+#     plt.title('U error')
+#     plt.show()
     
-    #plotting
-    plt.contourf(lambdas, mus, U)
-    plt.colorbar()
-    plt.title('U IC')
-    plt.show()
+#     #plotting
+#     plt.contourf(lambdas, mus, U)
+#     plt.colorbar()
+#     plt.title('U IC')
+#     plt.show()
     
-    plt.contourf(lambdas, mus, Unew)
-    plt.colorbar()
-    plt.title('Transform')
-    plt.show()
+#     plt.contourf(lambdas, mus, Unew)
+#     plt.colorbar()
+#     plt.title('Transform')
+#     plt.show()
     
-    return Unew, Vnew
+#     return Unew, Vnew
 ## This test takes vorticity and divergence, converts them to a wind field and recreates vorticity and divergence
 
 ## This is a test for the Laplacian
@@ -121,6 +135,6 @@ def wind_test(U,V,I,J,M,N,Pmn,Hmn,w,tstepcoeff,tstepcoeffmn,mJarray,marray,dt,fm
 ## This is a test from Liu and Showman 
 
 
-Unew=transform_test(Uic,I,J,M,N,Pmn,w,mus,lambdas)
+# Unew=transform_test(Uic,I,J,M,N,Pmn,w,mus,lambdas)
 
-Unew,Vnew=wind_test(Uic,Vic,I,J,M,N,Pmn,Hmn,w,tstepcoeff,tstepcoeffmn,mJarray,marray,dt,fmn)
+# Unew,Vnew=wind_test(Uic,Vic,I,J,M,N,Pmn,Hmn,w,tstepcoeff,tstepcoeffmn,mJarray,marray,dt,fmn)
