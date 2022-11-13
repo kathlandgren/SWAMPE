efa8c05dd387c1aa2d92c2a7e5122631088d1e84
kathlandgren <78450083+kathlandgren@users.noreply.github.com>
2022-11-13 16:15:22 -0700

    add reference data

---
+++ docs/hot_Jupiter_reference_example/readme.md
@@ -0,0 +1,6 @@
+This folder contains the scripts used to generate three simulations of a hot Jupiter. The scripts include both planetary and simulation parameters.
+
+Each simulation was run for 1500 h. The reference data provided here are snapshots at 1500 h. The simulations for taurad=0.1 days equilibrate at around 200 hours. The simulations for taurad=1 day and taurad=10 days equilibrate after approximately 1000 hours.
+
+These simulations took between 21 and 24 hours to complete while running simultaneously on a single core.
+
---
+++ docs/hot_Jupiter_reference_example/taurad_0p1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 11:44:57 2022
+
+@author: ezets
+"""
+
+import numpy
+import SWAMPE
+import time
+start_time = time.time()
+
+
+# planetary radius a in meters
+
+a=8.2*10**(7) 
+
+# planetary rotation rate omega in radians/s
+
+omega=3.2*10**(-5)
+
+# the reference geopotential height, m^2/s^2, typically based on scale height
+
+Phibar=4*(10**6)
+
+#timestep length in seconds
+dt=30
+#number of timesteps for the simulation
+tmax=180000
+
+M=42
+
+DPhieq=Phibar
+
+taurad=int(3600*24*0.1) 
+taudrag=-1
+
+plotflag=False
+
+SWAMPE.run_model(M,dt,tmax,Phibar, omega, a,  taurad=taurad, taudrag=taudrag, DPhieq=DPhieq, plotflag=plotflag,saveflag=True,savefreq=1200,verbose=True,timeunits='hours',K6=1.24*10**35) 
+
+print("--- %s seconds ---" % (time.time() - start_time))
